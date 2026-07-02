f5e7b9bcb3b6ad1a882638ff824272a8300020c0
Korlne <tjd592751534@gmail.com>
2026-07-02 15:50:02 +0800

    Add BuffGraph building block registry

---
+++ zsim/sim_progress/BuffGraph/adapters/__init__.py
@@ -0,0 +1,8 @@
+from .base import BuffGraphAdapter, BuffGraphAdapterContext, BuffGraphAdapterResult
+
+__all__ = [
+    "BuffGraphAdapter",
+    "BuffGraphAdapterContext",
+    "BuffGraphAdapterResult",
+]
+
